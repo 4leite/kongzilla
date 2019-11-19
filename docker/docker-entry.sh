6348a13be34f7c7fc3d6cb2a542e77c6ea88b279
Jon Vivian <jonv@datacom.co.nz>
2019-11-19 14:57:00 +1300

    serve off _kongzilla subdirectory

--- docker/docker-entry.sh
+++ docker/docker-entry.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 if [ "$1" == "run" ]; then
-    tee /usr/share/nginx/html/env.js << EOF > /dev/null
+    tee /usr/share/nginx/html/_kongzilla/env.js << EOF > /dev/null
 (function(window) {
 	window.env = window.env || {} 
 	window.env.API_ROOT = "${API_HOST:-http://localhost:8001}";
