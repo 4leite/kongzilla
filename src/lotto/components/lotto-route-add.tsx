674dffb3dc17e7cbc774ecbfd63e18b02f181b53
Jon Vivian <jonv@datacom.co.nz>
2020-02-19 16:59:14 +1300

    fix host box - change to autocomplete input

--- src/lotto/components/lotto-route-add.tsx
+++ src/lotto/components/lotto-route-add.tsx
@@ -2,7 +2,8 @@ import React, { useState } from 'react'
 import styled from 'styled-components'
 import { useStoreState, useStoreActions } from 'lotto/model'
 import { Method } from 'shared/model/method'
-import CreatableSelect from 'react-select/creatable';
+import { Autocomplete } from 'shared/components/autocomplete'
+// import CreatableSelect from 'react-select/creatable';
 
 const Priority = styled.input`
 	width: 2em;
@@ -19,7 +20,21 @@ const ErrorMessage = styled.div`
 	justify-self: center;
 	color: red;
 `
-
+/*
+const SelectWrapper = styled.div`
+	padding-right: 20px;
+`
+const SelectList = styled.select`
+    width: 150px;
+	height: 30px;
+`
+const SelectInput = styled.input`
+    width: 130px;
+    margin-left: -148px;
+    height: 25px;
+    border: none;
+`
+*/
 const onChange = (setState: (value: any) => void) => (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
 	e.preventDefault()
 	setState(e.currentTarget.value)
@@ -29,7 +44,7 @@ const onChangeParseInt = (setState: (value: any) => void) => (e: React.ChangeEve
 	e.preventDefault()
 	setState(parseInt(e.currentTarget.value))
 }
-
+/*
 const onChangeCreatable = (setState: (value: any) => void) => (newValue: any, actionMeta: any) => {
 	console.log('change host: ', newValue?.value)
 	setState(newValue?.value)
@@ -39,35 +54,30 @@ const onAddCreatable = (setState: (value: any) => void) => (newValue: any, actio
 	console.log('add host: ', newValue)
 	// setState(newValue)
 };
+*/
 
 export const LottoRouteAdd: React.FC = () => {
 	const setSelected = useStoreActions(action => action.routes.setSelected)
 
 	const readHosts = useStoreState(state=> state.hosts.resource.read)
-
-	const hosts = readHosts().map(host => (
-		{value: host.name, label: host.name}
-	))
+	const hosts = readHosts().map(host => host.name)
 
 	const [error, setError] = useState()
 	const [priority, setPriority] = useState(10)
 	const [path, setPath] = useState('/api/')
 	const [methods, setMethods] = useState('GET, PUT, POST, DELETE')
-	const [destination, setDestination] = useState(null);
+	const [destination, setDestination] = useState(hosts[0]);
+//	const [hostId, setHostId] = useState(0);
 
 	const isDisabled: boolean = useStoreState(state => state.routes.resource.isFetching)
 	const addRouteAction = useStoreActions(actions => actions.routes.addRoute)
+	const updateHosts = useStoreActions(actions => actions.hosts.resource.fetch)
 
 	const onClick = (e: React.SyntheticEvent) => {
 		e.preventDefault()
 		setError(null)
 		
 		// TODO: better validations
-		if (!destination) {
-			setError(Error('Please select a destination'))
-			return
-		}
-
 		addRouteAction({
 			priority,
 			request: {
@@ -75,11 +85,11 @@ export const LottoRouteAdd: React.FC = () => {
 				path
 			},
 			response: {
-				// how to tell typescript I've checked for null above?
-				host: destination ?? ''
+				host: destination
 			}
 		})
 		.then((response: string) => {
+			updateHosts()
 			setSelected(response)
 		})
 		.catch((error: Error) => {
@@ -90,6 +100,25 @@ export const LottoRouteAdd: React.FC = () => {
 	return <>
 		<Priority name='priority' onChange={onChangeParseInt(setPriority)} value={priority}/>
 		<Path name='path' onChange={onChange(setPath)} value={path}/>
+		<Autocomplete
+			value={destination}
+			setValue={setDestination}
+			suggestions={hosts}
+		/>
+{/*
+		<SelectWrapper>
+			<SelectList value={hostId} onChange={onChange((id: any) => {
+					setDestination(hosts[id].name)
+					setHostId(id)
+			})}>
+				{hosts.map((host, index) => (
+					<option key={host.id} value={index}>{host.name}</option>
+				))}
+			</SelectList>
+			<SelectInput value={destination} onChange={onChange(setDestination)} name={'aname'} maxLength={5}/>
+		</SelectWrapper>
+*/}
+{/*
 		<CreatableSelect 
 			isClearable
 			name='destination' 
@@ -97,6 +126,7 @@ export const LottoRouteAdd: React.FC = () => {
 			onInputChange={onAddCreatable(setDestination)}
 			options={hosts}
 		/>
+*/}
 		<Methods name='methods' onChange={onChange(setMethods)} value={methods}/>
 		<Actions>
 			<button name='add' onClick={onClick} disabled={isDisabled}>Add</button>
---
+++ src/shared/components/autocomplete/index.tsx
@@ -0,0 +1,126 @@
+import React, { useState, useCallback } from "react";
+import styled from 'styled-components';
+
+// class="suggestions"
+const Suggestions = styled.ul`
+	list-style: none;
+	margin-top: 0;
+	max-height: 143px;
+	overflow-y: auto;
+	padding-left: 0;
+	position: absolute;
+`
+const highlighted = `
+	background-color: #bbb;
+  	color: #000;
+  	cursor: pointer;
+`
+const ActiveSuggestion = styled.li`
+	padding: 2px;
+	${highlighted}
+`
+const Suggestion = styled.li`
+	padding: 2px;
+	background-color: #eee;
+	:hover {${highlighted}}
+`
+
+interface Props {
+	value: string
+	setValue: (value: string) => void
+	suggestions: string[]
+}
+
+export const Autocomplete: React.FC<Props> = (props) => {
+
+	const { value, setValue, suggestions } = props
+
+	const [activeSuggestion, setActiveSuggestion] = useState(0)
+	const [filteredSuggestions, setFilteredSuggestions] = useState(suggestions)
+	const [showSuggestions, setShowSuggestions] = useState(false)
+	const [position, setPosition] = useState({})
+
+	// Event fired when the user clicks on a suggestion
+	const onFocus = (e: React.FocusEvent<HTMLElement>) => {
+		setFilteredSuggestions(suggestions)
+		setActiveSuggestion(0)
+		setShowSuggestions(true)
+	}
+	// Hide the suggestions if the user moves away without selecting
+	const onBlur = (e: React.FocusEvent<HTMLElement>) => {
+		setShowSuggestions(false)
+	}
+	// Event fired when the input value is changed
+	const onChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+		const userInput = e.currentTarget.value
+
+		// Filter our suggestions that don't contain the user's input
+		setFilteredSuggestions(suggestions.filter(suggestion =>
+			suggestion.toLowerCase().indexOf(userInput.toLowerCase()) > -1
+		))
+		setValue(e.currentTarget.value)
+		setActiveSuggestion(0)
+		setShowSuggestions(true)
+	}
+	// Event fired when the user presses a key down
+	const onKeyDown = (e: React.KeyboardEvent) => {
+		// User pressed the enter key, update the input and close the
+		// suggestions
+		if (e.keyCode === 13) {
+			setValue(filteredSuggestions[activeSuggestion])
+			setActiveSuggestion(0)
+			setShowSuggestions(false)
+		}
+		// User pressed the up arrow, decrement the index
+		else if (e.keyCode === 38) {
+			if (activeSuggestion > 0) {
+				setActiveSuggestion(activeSuggestion - 1)
+			}
+		}
+		// User pressed the down arrow, increment the index
+		else if (e.keyCode === 40) {
+			if (activeSuggestion < filteredSuggestions.length - 1) {
+				setActiveSuggestion(activeSuggestion + 1)
+			}
+		}
+	}
+	// Event fired when the user clicks on a suggestion
+	// NB: OnClick conflicts with Onblur above
+	const onMouseDown = (e: React.MouseEvent<HTMLElement>) => {
+		setFilteredSuggestions(suggestions)
+		setValue(e.currentTarget.innerText)
+		setActiveSuggestion(0)
+		setShowSuggestions(true)
+	}
+	// Set position of suggestions
+	const inputCallback = useCallback((node: HTMLInputElement) => {
+		setPosition({
+			left: node.offsetLeft,
+			top: node.offsetTop + node.offsetHeight
+		})
+	}, [setPosition])
+
+	return <>
+		<input
+			type="text"
+			ref={inputCallback}
+			onFocus={onFocus}
+			onBlur={onBlur}
+			onChange={onChange}
+			onKeyDown={onKeyDown}
+			value={value}
+		/>
+		{showSuggestions && <Suggestions style={position}>
+			{filteredSuggestions.map(
+				(suggestion, index) => (index === activeSuggestion) ? 
+					<ActiveSuggestion key={suggestion} onMouseDown={onMouseDown}>
+						{suggestion}
+					</ActiveSuggestion>
+					:
+					<Suggestion key={suggestion} onMouseDown={onMouseDown}>
+						{suggestion}
+					</Suggestion>
+			)}
+		</Suggestions>}
+	</>
+}
